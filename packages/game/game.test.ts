393d867a33dd9d674049dd509c985152e2b1b83e
Pap Norbert <norbert.program306@gmail.com>
2026-06-11 16:54:02 +0200

    Revert "feat: setup base game state blueprint and initial player factory"

--- packages/game/game.test.ts
+++ packages/game/game.test.ts
@@ -1,12 +1,7 @@
-import '@testing-library/jest-dom';
+import { setupGame } from './game';
 
-import { Ludo } from './game';
-import { setupLudo } from './setup';
-
-describe('Core Game Object', () => {
-  test('should export the Ludo game object with correct name and setup function', () => {
-    expect(Ludo.name).toBe('Ne ljuti se čoveče!');
-
-    expect(Ludo.setup).toBe(setupLudo);
+describe('Ludo Game Setup', () => {
+  it('should initialize the game', () => {
+    expect(setupGame()).toBe('Game is ready!');
   });
 });
--- packages/game/game.ts
+++ packages/game/game.ts
@@ -1,9 +1,3 @@
-import { type Game } from 'boardgame.io';
-
-import { setupLudo } from './setup';
-import { type LudoGameState } from './types';
-
-export const Ludo: Game<LudoGameState> = {
-  name: 'Ne ljuti se čoveče!',
-  setup: setupLudo,
-};
+export function setupGame() {
+  return 'Game is ready!';
+}
